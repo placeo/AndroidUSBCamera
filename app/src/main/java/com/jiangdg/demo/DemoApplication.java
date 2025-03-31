b0ec85731b63d8046dfdff58b84f3b1e391f4cec
Youngkeun Kim <youngkeun.kim@sk.com>
2025-03-31 15:17:44 +0900

    REFACTOR: change DemoApplication from kotlin to java

--- app/src/main/java/com/jiangdg/demo/DemoApplication.java
+++ app/src/main/java/com/jiangdg/demo/DemoApplication.java
@@ -7,25 +7,26 @@
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jiangdg.demo
+package com.jiangdg.demo;
 
-import android.app.Application
-import timber.log.Timber
+import android.app.Application;
+import timber.log.Timber;
 
 /**
  *
- * @author Created by jiangdg on 2022/2/28
+ * Created by jiangdg on 2022/2/28
  */
-class DemoApplication: Application() {
+public class DemoApplication extends Application {
 
-    override fun onCreate() {
-        super.onCreate()
-        Timber.plant(Timber.DebugTree())
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Timber.plant(new Timber.DebugTree());
     }
 }
